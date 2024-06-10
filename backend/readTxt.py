5389ee01e67d076a5aaf22e93431d7e598fc8561
MarleneSchiappaa <max.tonnadre@efrei.net>
2024-06-10 10:08:25 +0200

    +

--- backend/readTxt.py
+++ backend/readTxt.py
@@ -1,9 +1,9 @@
-#from  backend.classe.gare import Gare
+#from  classe.gare import Gare
 #from  backend.classe.voie import Voie
-from backend.classe.line import *
+from classe.line import Line
 import re
 
-def ReadTxt():
+def ReadTxtPoints():
     file=open('backend/data/metro.txt','r')
     pattern = re.compile(r'^V \d{4}')
     lines=file.readlines()
@@ -12,12 +12,41 @@ def ReadTxt():
         #print(line)
     return filtered_lines
 
-def CreateAndFillLine(LineOfTxt):
-    for line in LineOfTxt:
-        line = line.split()
-        line_name = line[2]
-        newLine = Line(line_name)
-        
+def ReadTxtConnexions():
+    file=open('backend/data/connexions.txt','r')
+    pattern = re.compile(r'^E \d')
+    lines=file.readlines()
+    filtered_lines = [line for line in lines if pattern.match(line)]
+    #for line in filtered_lines:
+        #print(line)
+    return filtered_lines
 
-LineOfTxt=ReadTxt()
-CreateAndFillLine(LineOfTxt)
+def CreateAndFillLine(LineOfTxt):
+    newLines=[]
+    for line in LineOfTxt:
+        line = line.split(";")[1]
+        line_name = line.replace(' ','')
+        newLines.append(Line(line_name))
+        #print(line_name)
+
+def FillLineWithGare(LineOfTxt,NewLines):
+    for line in LineOfTxt:
+        line = line.split(";")
+        line_name = line[1].replace(' ','')
+        for NewLine in NewLines:
+            if NewLine.get_attr('name') == line_name:
+                print(line_name)
+                #getGare 
+                copyLine=line[0]
+                copyLine=copyLine.split(" ")
+                gare_name=""
+                for i in range(2,len(copyLine)):
+                    gare_name += copyLine[i]
+                    print(gare_name)
+                #getCoord
+                
+                    
+if __name__ == "__main__":   
+    LineOfTxt=ReadTxtPoints()
+    NewLines=CreateAndFillLine(LineOfTxt)
+    
