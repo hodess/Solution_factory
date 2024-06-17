0fcfb4423a0dae31c7a44b0eefbc2726d0dec5c7
Thimoclesse <39627184+Thimoclesse@users.noreply.github.com>
2024-06-18 01:30:59 +0200

    Ajout de la barre de recherche avec les logo de lignes

--- vue-demo/vue-demo/src/components/DepartDestination.vue
+++ vue-demo/vue-demo/src/components/DepartDestination.vue
@@ -1,91 +1,161 @@
 <script setup>
-import { stations } from "@/stockage/gares";
-import { ref, computed } from 'vue';
+  import { ref, computed } from 'vue';
+  import { stations } from "@/stockage/gares";
 
-function useSearch(stations) {
-  const searchText = ref('');
-  const searchResults = computed(() => {
-    if (!searchText.value) {
-      return [];
-    }
-    return stations.filter(station =>
-      station.name.toLowerCase().includes(searchText.value.toLowerCase()) ||
-      station.address.toLowerCase().includes(searchText.value.toLowerCase())
-    ).slice(0, 4); // Limite à 2 résultats
-  });
+  const { searchText: searchTextDepart, searchResults: searchResultsDepart } = useSearch(stations);
+  const { searchText: searchTextArrivee, searchResults: searchResultsArrivee } = useSearch(stations);
 
-  return {
-    searchText,
-    searchResults,
-  };
-}
+  const isDepartFocused = ref(false);
+  const isArriveeFocused = ref(false);
 
-const { searchText: searchTextDepart, searchResults: searchResultsDepart } = useSearch(stations);
-const { searchText: searchTextArrivee, searchResults: searchResultsArrivee } = useSearch(stations);
+  function useSearch(stations) {
+    const searchText = ref('');
+    const searchResults = computed(() => {
+      if (!searchText.value) {
+        return [];
+      }
+      return stations.filter(station =>
+          station.name.toLowerCase().includes(searchText.value.toLowerCase()) ||
+          station.address.toLowerCase().includes(searchText.value.toLowerCase())
+      ).slice(0, 4);
+    });
 
-const isDepartFocused = ref(false);
-const isArriveeFocused = ref(false);
-
-function selectStation(station, type) {
-  if (type === 'depart') {
-    searchTextDepart.value = station.name;
-    isDepartFocused.value = false;
-    // Supprimer la gare sélectionnée de searchResultsDepart pour éviter la duplication
-    searchResultsDepart.value = searchResultsDepart.value.filter(result => result.name !== station.name);
-  } else if (type === 'arrivee') {
-    searchTextArrivee.value = station.name;
-    isArriveeFocused.value = false;
-    // Supprimer la gare sélectionnée de searchResultsArrivee pour éviter la duplication
-    searchResultsArrivee.value = searchResultsArrivee.value.filter(result => result.name !== station.name);
+    return {
+      searchText,
+      searchResults,
+    };
   }
-  return{
-    searchTextDepart,
-    searchTextArrivee,
-  };
-}
+
+  function selectStation(station, type) {
+    if (type === 'depart') {
+      searchTextDepart.value = station.name;
+      isDepartFocused.value = false;
+      searchResultsDepart.value = searchResultsDepart.value.filter(result => result.name !== station.name);
+    } else if (type === 'arrivee') {
+      searchTextArrivee.value = station.name;
+      isArriveeFocused.value = false;
+      searchResultsArrivee.value = searchResultsArrivee.value.filter(result => result.name !== station.name);
+    }
+  }
+
+  function getStationLines(lines) {
+    const lineMap = {
+      'M1': 'src/components/lines/metro-1.png',
+      'M2': 'src/components/lines/metro-2.png',
+      'M3': 'src/components/lines/metro-3.png',
+      'M4': 'src/components/lines/metro-4.png',
+      'M5': 'src/components/lines/metro-5.png',
+      'M6': 'src/components/lines/metro-6.png',
+      'M7': 'src/components/lines/metro-7.png',
+      'M8': 'src/components/lines/metro-8.png',
+      'M9': 'src/components/lines/metro-9.png',
+      'M10': 'src/components/lines/metro-10.png',
+      'M11': 'src/components/lines/metro-11.png',
+      'M12': 'src/components/lines/metro-12.png',
+      'M13': 'src/components/lines/metro-13.png',
+      'M14': 'src/components/lines/metro-14.png',
+      'RER A': 'src/components/lines/rer-a.png',
+      'RER B': 'src/components/lines/rer-b.png',
+      'RER C': 'src/components/lines/rer-c.png',
+      'RER D': 'src/components/lines/rer-d.png',
+      'RER E': 'src/components/lines/rer-e.png'
+    };
+
+    const metroLines = [];
+    const rerLines = [];
+    lines.split(', ').map(l => l.trim()).forEach(l => {
+      if (lineMap[l]) {
+        if (l.startsWith('M')) {
+          metroLines.push(lineMap[l]);
+        } else if (l.startsWith('RER')) {
+          rerLines.push(lineMap[l]);
+        }
+      } else {
+        console.log(`Image not found for line: ${l}`);
+      }
+    });
+
+    return {
+      metroLines,
+      rerLines
+    };
+  }
+
+
 </script>
 
 <template>
   <div class="block_input">
     <div class="title-trip">Où voulez-vous aller ?</div>
+
+    <!-- Departure input -->
     <div class="small-title">De :</div>
     <input
-      :value="searchTextDepart"
-      @input="searchTextDepart = $event.target.value"
-      @focus="isDepartFocused = true"
-      @blur="() => setTimeout(() => isDepartFocused = false, 100)"
-      type="search"
-      class="search"
-      placeholder="Gare de départ"
+        v-model="searchTextDepart"
+        @focus="isDepartFocused = true"
+        @blur="() => setTimeout(() => isDepartFocused = false, 100)"
+        type="search"
+        class="search"
+        placeholder="Gare de départ"
     />
-    <ul v-if="isDepartFocused && searchResultsDepart.length">
-      <li v-for="result in searchResultsDepart" :key="result.name" @click="selectStation(result, 'depart')">
-        <strong>{{ result.name }}</strong><br>
-        {{ console.log(searchTextDepart) }}
 
+    <ul v-if="isDepartFocused && searchResultsDepart.length">
+      <li
+          v-for="result in searchResultsDepart"
+          :key="result.name"
+          @click="selectStation(result, 'depart')"
+          :class="{ 'double-height': getStationLines(result.line).metroLines.length && getStationLines(result.line).rerLines.length }"
+      >
+        <strong>{{ result.name }}</strong><br>
+        <div class="lines-container">
+          <div class="metro-lines" v-if="getStationLines(result.line).metroLines.length">
+            <template v-for="image in getStationLines(result.line).metroLines">
+              <div class="logo-line" :style="{ backgroundImage: `url(${image})` }"></div>
+            </template>
+          </div>
+          <div class="rer-lines" v-if="getStationLines(result.line).rerLines.length">
+            <template v-for="image in getStationLines(result.line).rerLines">
+              <div class="logo-line" :style="{ backgroundImage: `url(${image})` }"></div>
+            </template>
+          </div>
+        </div>
       </li>
     </ul>
 
-    <div class="small-title">A :</div>
+    <!-- Arrival input -->
+    <div class="small-title">À :</div>
     <input
-      :value="searchTextArrivee"
-      @input="searchTextArrivee = $event.target.value"
-      @focus="isArriveeFocused = true"
-      @blur="() => setTimeout(() => isArriveeFocused = false, 100)"
-      type="search"
-      class="search"
-      placeholder="Gare d'arrivée"
+        v-model="searchTextArrivee"
+        @focus="isArriveeFocused = true"
+        @blur="() => setTimeout(() => isArriveeFocused = false, 100)"
+        type="search"
+        class="search"
+        placeholder="Gare d'arrivée"
     />
     <ul v-if="isArriveeFocused && searchResultsArrivee.length">
-      <li v-for="result in searchResultsArrivee" :key="result.name" @click="selectStation(result, 'arrivee')">
+      <li
+          v-for="result in searchResultsArrivee"
+          :key="result.name"
+          @click="selectStation(result, 'arrivee')"
+          :class="{ 'double-height': getStationLines(result.line).metroLines.length && getStationLines(result.line).rerLines.length }"
+      >
         <strong>{{ result.name }}</strong><br>
-        {{ console.log(searchTextArrivee) }}
-
+        <div class="lines-container">
+          <div class="metro-lines" v-if="getStationLines(result.line).metroLines.length">
+            <template v-for="image in getStationLines(result.line).metroLines">
+              <div class="logo-line" :style="{ backgroundImage: `url(${image})` }"></div>
+            </template>
+          </div>
+          <div class="rer-lines" v-if="getStationLines(result.line).rerLines.length">
+            <template v-for="image in getStationLines(result.line).rerLines">
+              <div class="logo-line" :style="{ backgroundImage: `url(${image})` }"></div>
+            </template>
+          </div>
+        </div>
       </li>
     </ul>
   </div>
 </template>
-
 <style scoped>
 .search {
   flex-grow: 1;
@@ -126,7 +196,7 @@ ul {
   padding: 0;
   margin-top: 1rem;
   background-color: #fff;
-  border-radius: 1rem;
+  border-radius: 0.7rem;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 }
 
@@ -134,10 +204,46 @@ li {
   padding: 0.5rem 1rem;
   border-bottom: 1px solid #ddd;
   color: black;
-  cursor: pointer; /* Ajout d'un curseur pointeur pour indiquer que l'élément est cliquable */
+  cursor: pointer;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 }
 
 li:last-child {
   border-bottom: none;
 }
+
+strong{
+  min-width: 50%;
+}
+
+.logo-line {
+  background-size: cover;
+  background-repeat: no-repeat;
+  width: 1.25rem;
+  height: 1.25rem;
+  margin-top: 3px;
+  margin-right: 3px;
+  margin-bottom: 3px;
+}
+
+
+.lines-container {
+  display: flex;
+  flex-direction: column;
+}
+
+.metro-lines,
+.rer-lines {
+  display: flex;
+}
+
+
+
+.double-height {
+  height: 4rem; /* Adjust this value as needed */
+}
+
 </style>
+
--- vue-demo/vue-demo/src/stockage/gares.js
+++ vue-demo/vue-demo/src/stockage/gares.js
@@ -1,9 +1,12 @@
 import {ref} from "vue";
 
 export const stations = [
-    { name: 'Villejuif - Louis Aragon', address: '23, Avenue de Stalingrad, 94800, Villejuif', line: 'M7, T7', bus: '162, 172, 180, 185, 286, 380, V7', noctilien: 'N15, N22' },
+    { name: 'Villejuif - Louis Aragon', address: '23, Avenue de Stalingrad, 94800, Villejuif', line: 'M7', bus: '162, 172, 180, 185, 286, 380, V7', noctilien: 'N15, N22' },
     { name: 'Villeneuve-saint-georges', address: 'Port de Villeneuve-Saint-Georges, 94190, Villeneuve-Saint-Georges', line: 'RER D', noctilien: 'N132, N134, N135' },
     { name: 'Villeparisis - Mitry-le-neuf', address: '77290, Mitry-Mory', line: 'RER B', noctilien: 'N41' },
-    { name: 'Villepreux - Les Clayes', address: 'Avenue Henri Barbusse, 78340, Les Clayes-sous-Bois', line: 'N' },
-    { name: 'Villejuif Léo Lagrange', address: '46, Avenue de Paris, 94800, Villejuif', line: 'M7' }
+    { name: 'Villepreux - Les Clayes', address: 'Avenue Henri Barbusse, 78340, Les Clayes-sous-Bois', line: 'RER A' },
+    { name: 'Villejuif Léo Lagrange', address: '46, Avenue de Paris, 94800, Villejuif', line: 'M7' },
+    { name: 'Mairie des Lilas', address: '46, Avenue de Paris, 94800, Villejuif', line: 'M11' },
+    { name: 'Chatelet - Les Halles', address: '46, Avenue de Paris, 94800, Villejuif', line: 'M1, M4, M7, M11, M14, RER A, RER B, RER D' }
   ];
+
