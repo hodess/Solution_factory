f0329b57b61b9a579669403beaff7683cb116c5d
Eugenienakouzi <94555318+Eugenienakouzi@users.noreply.github.com>
2024-06-17 14:06:50 +0200

    completion fini

--- vue-demo/vue-demo/src/stockage/gares.js
+++ vue-demo/vue-demo/src/stockage/gares.js
@@ -1,3 +1,9 @@
 import {ref} from "vue";
 
-export const gares = ref(["Gare A", "Gare B", "Gare C", "Gare D"]);
+export const stations = [
+    { name: 'Villejuif - Louis Aragon', address: '23, Avenue de Stalingrad, 94800, Villejuif', line: 'M7, T7', bus: '162, 172, 180, 185, 286, 380, V7', noctilien: 'N15, N22' },
+    { name: 'Villeneuve-saint-georges', address: 'Port de Villeneuve-Saint-Georges, 94190, Villeneuve-Saint-Georges', line: 'RER D', noctilien: 'N132, N134, N135' },
+    { name: 'Villeparisis - Mitry-le-neuf', address: '77290, Mitry-Mory', line: 'RER B', noctilien: 'N41' },
+    { name: 'Villepreux - Les Clayes', address: 'Avenue Henri Barbusse, 78340, Les Clayes-sous-Bois', line: 'N' },
+    { name: 'Villejuif Léo Lagrange', address: '46, Avenue de Paris, 94800, Villejuif', line: 'M7' }
+  ];
