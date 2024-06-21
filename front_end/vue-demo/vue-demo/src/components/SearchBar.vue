d2c7f9f14c21f67b8dd149b6f42835c115c9bb78
Eugenienakouzi <94555318+Eugenienakouzi@users.noreply.github.com>
2024-06-21 10:43:17 +0200

    esaaie affichage console bouton

--- front_end/vue-demo/vue-demo/src/components/SearchBar.vue
+++ front_end/vue-demo/vue-demo/src/components/SearchBar.vue
@@ -1,11 +1,28 @@
   <script setup>
-  import { ref } from 'vue';
+  import { ref, onMounted } from 'vue';
   import axios from 'axios'; // Assurez-vous d'importer axios si ce n'est pas déjà fait
 
   // Vos imports de composants ici
   import DepartDestination from "@/components/DepartDestination.vue";
   import HeureDepart from './HeureDepart.vue';
   import HeureArrivee from './HeureArrivee.vue';
+
+  const svgElementRef = ref(null);
+
+  let Depart = ref(DepartDestination.searchTextDepart);
+  let Arrivee = ref(DepartDestination.searchTextArrive);
+
+  onMounted(() => {
+  if (svgElementRef.value) {
+    svgElementRef.value.addEventListener("click", function() {
+      console.log(DepartDestination)
+      console.log(Depart.value);
+      console.log(Arrivee);
+
+    });
+  }
+});
+
 
   const selectedTime = ref('depart');
 
@@ -71,12 +88,11 @@
         <!-- <HeureArrivee/> -->
 
         <div class="wrapper">
-          <div class="link_wrapper">
-            <a href="#">Démarrer</a>
+          <div class="link_wrapper" ref="svgElementRef" id="mySVG">
+            <a href="#" ref="svgElementRef" id="mySVG">Démarrer</a>
             <div class="icon">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 268.832 268.832">
+            <svg   xmlns="http://www.w3.org/2000/svg" viewBox="0 0 268.832 268.832">
               <path d="M265.17 125.577l-80-80c-4.88-4.88-12.796-4.88-17.677 0-4.882 4.882-4.882 12.796 0 17.678l58.66 58.66H12.5c-6.903 0-12.5 5.598-12.5 12.5 0 6.903 5.597 12.5 12.5 12.5h213.654l-58.66 58.662c-4.88 4.882-4.88 12.796 0 17.678 2.44 2.44 5.64 3.66 8.84 3.66s6.398-1.22 8.84-3.66l79.997-80c4.883-4.882 4.883-12.796 0-17.678z"/>
-
             </svg>
             </div>
           </div>
