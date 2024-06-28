53af25c0b57c3a11beabd4329cc8b4e4592adcf7
romain dupont <66488197+hodess@users.noreply.github.com>
2024-06-28 12:30:11 +0200

    ajout des rers

--- back_end/back_end/src/main/java/classe/Fonction.java
+++ back_end/back_end/src/main/java/classe/Fonction.java
@@ -43,4 +43,10 @@ public class Fonction {
         return connexite.is_connected_dfs(start_gare,linesMap);
     }
 
+    public static void main(String[] args) {
+            Create_class.create_all_class();
+            String start = "Aimé Césaire";
+            String end = "Bibliothèque François Mitterrand";
+            System.out.println(find_chemin_start_end(start, end));
+        }
 }
