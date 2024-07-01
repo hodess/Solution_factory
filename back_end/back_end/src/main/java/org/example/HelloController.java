1f4555f8311290725c04558b6b9baa9347feeb66
romain dupont <66488197+hodess@users.noreply.github.com>
2024-07-01 16:12:05 +0200

    ajout du temps

--- back_end/back_end/src/main/java/org/example/HelloController.java
+++ back_end/back_end/src/main/java/org/example/HelloController.java
@@ -4,6 +4,7 @@ import classe.*;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,48 +17,104 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
         logger.info("hello");
-        return "hello world!";
+        String response = "hello world!";
+
+        stopWatch.stop();
+        logger.info("Execution time of sayHello: {} ms", stopWatch.getTotalTimeMillis());
+
+        return response;
     }
 
     @GetMapping("/init")
     public String callGareDatabase() {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
         logger.info("init");
         Create_class.create_all_class();
-        return "create_all_class() initialized";
+        String response = "create_all_class() initialized";
+
+        stopWatch.stop();
+        logger.info("Execution time of callGareDatabase: {} ms", stopWatch.getTotalTimeMillis());
+
+        return response;
     }
 
     @GetMapping("/find_gare")
     public String find_chemin(@RequestParam String start, @RequestParam String end) {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
         System.out.println("start: " + start + " end: " + end);
         Create_class.create_all_class();
         logger.info("find gare");
-        return Fonction.find_chemin_start_end(start, end);
+        String response = Fonction.find_chemin_start_end(start, end);
+
+        stopWatch.stop();
+        logger.info("Execution time of find_chemin: {} ms", stopWatch.getTotalTimeMillis());
+
+        return response;
     }
 
     @GetMapping("/gares")
     public String all_gares() {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
         logger.info("toute les gares");
-        return ReturnGareNoTraited.CallFunctions();
+        String response = ReturnGareNoTraited.CallFunctions();
+
+        stopWatch.stop();
+        logger.info("Execution time of all_gares: {} ms", stopWatch.getTotalTimeMillis());
+
+        return response;
     }
 
     @GetMapping("/connexite")
     public boolean connexite() {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
         logger.info("connexité");
-        return Fonction.main_connexite();
+        boolean response = Fonction.main_connexite();
+
+        stopWatch.stop();
+        logger.info("Execution time of connexite: {} ms", stopWatch.getTotalTimeMillis());
+
+        return response;
     }
 
     @GetMapping("/garesMap")
     public String all_garesMap() {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
         Create_class.create_all_class();
         logger.info("toute les gares");
-        return JsonConverter.convertObjectToJson(ReturnGareWithOrder.ReturnVue_lieu_with_line_and_order_());
+        String response = JsonConverter.convertObjectToJson(ReturnGareWithOrder.ReturnVue_lieu_with_line_and_order_());
+
+        stopWatch.stop();
+        logger.info("Execution time of all_garesMap: {} ms", stopWatch.getTotalTimeMillis());
+
+        return response;
     }
 
     @GetMapping("/multiply/{num1}/{num2}")
     public double multiplyNumbers(@PathVariable double num1, @PathVariable double num2) {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
         logger.info("Multiplying numbers: {} and {}", num1, num2);
-        return num1 * num2;
+        double result = num1 * num2;
+
+        stopWatch.stop();
+        logger.info("Execution time of multiplyNumbers: {} ms", stopWatch.getTotalTimeMillis());
+
+        return result;
     }
 
 }
